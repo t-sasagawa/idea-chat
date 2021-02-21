c2c420360e5c016c6923a236dad487a8c2f1bd96
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-02-21 17:53:21 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @room = Room.find(params[:room_id])
     @rooms = Room.all.includes(:user).order("created_at DESC")
+    @message = Message.new
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    # binding.pry
+    params.require(:message).permit(:content).merge(user_id: current_user.id)#, room_id: params[:room_id])
   end
 end
